c10ea748836b2229629e4859b75b3185a24c013b
jignesh133 <jignesh133@gmail.com>
2017-09-09 19:02:16 +0530

    Update UIImageView+Utils.h

--- FullScreenProfilePic/FullScreenProfilePic/UIImageView+Utils.h
+++ FullScreenProfilePic/FullScreenProfilePic/UIImageView+Utils.h
@@ -3,15 +3,19 @@
 @interface UIImageView (Utils)<UIGestureRecognizerDelegate,UIScrollViewDelegate>
 
 @property(nonatomic,strong)UIImageView *fullimageview;
-@property(nonatomic,strong)UIImageView *temptumb;
 @property(nonatomic,strong)UIScrollView *scrlView;
 @property(nonatomic,strong)UIView *viewbg;
+
 @property(nonatomic,strong)UIView *view;
+
 @property(nonatomic,strong)NSNumber *xPos;
 @property(nonatomic,strong)NSNumber *yPos;
 
 
 // METHOD FOR INITILIZE
+// USED WHEN YOU HAVE TO PASS WITH YOUR VIEW
 -(void)methodPhotoViewerWithSuperView:(UIView *)view;
 
+// IT WILL SHOW TOP OF THE VIEW
+-(void)methodPhotoViewer;
 @end
