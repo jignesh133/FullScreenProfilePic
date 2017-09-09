0b860ca7e6d8d2d6f452402966b4cde8322418f0
jignesh133 <jignesh133@gmail.com>
2017-09-09 17:40:45 +0530

    Update UIImageView+Utils.h

--- FullScreenProfilePic/FullScreenProfilePic/UIImageView+Utils.h
+++ FullScreenProfilePic/FullScreenProfilePic/UIImageView+Utils.h
@@ -1,11 +1,15 @@
 #import <UIKit/UIKit.h>
 
-@interface UIImageView (Utils)
+@interface UIImageView (Utils)<UIGestureRecognizerDelegate,UIScrollViewDelegate>
 
 @property(nonatomic,strong)UIImageView *fullimageview;
 @property(nonatomic,strong)UIImageView *temptumb;
+@property(nonatomic,strong)UIScrollView *scrlView;
 @property(nonatomic,strong)UIView *viewbg;
 @property(nonatomic,strong)UIView *view;
+@property(nonatomic,strong)NSNumber *xPos;
+@property(nonatomic,strong)NSNumber *yPos;
+
 
 // METHOD FOR INITILIZE
 -(void)methodPhotoViewerWithSuperView:(UIView *)view;
